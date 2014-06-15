86a220a12cb658a5f228564fe5e6bc95ab9451d5
Azure <302886512@qq.com>
2014-06-15 19:32:11 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '950770b64263fb8749badc805ee2e11549a8e48437e52f65926fcfdb6939a7ef1012c96f986cb0ad9bd88785bc92010461aa5d0c4e4194fb1e058fdc0506e0d1'
